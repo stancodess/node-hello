ba323cd4593913ba7cee40d5871d3fc7ecfca589
Stalin Caiche <stalin.caiche@gmail.com>
2024-01-21 14:36:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 const mongoConnectionString = process.env.MONGO_URL || 'variable de entorno no está definida';;
 
 app.get('/', (req, res) => {
-  res.send`Stalin Caiche: Connection String de MongoDB: ${mongoConnectionString}`);
+  res.send(`Stalin Caiche: Connection String de MongoDB: ${mongoConnectionString}`);
 });
 
 app.listen(port, () => {
